0cb4e7d05a0c529c5d1a4d77e7a99151f39466cb
klemus8 <klemush8@gmail.com>
2025-01-31 17:27:11 -0600

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,7 +4,12 @@ public class DemoDay2 {
 
 	public static void main(String[] args) {
 		System.out.println("Batch 13 Day 2 Git Lecture");
-		
+
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+
+		//this is coming from github
 		
 	}
 }
